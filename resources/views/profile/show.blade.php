fad633aec6861eda3a2d6f9084059ec9ea78cc83
Wadinga Leonard Ngonga <wadingaleonard@gmail.com>
2023-02-03 17:35:50 +0100

    Removed two factor auth for users

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -19,7 +19,6 @@
                             Security/Account Deletion
                         </div>
                         <div class="card-body">
-
                             @if (Session::has('message'))
                                 <div class="row">
                                     <div class="col-lg-12">
@@ -45,23 +44,7 @@
                                     </div>
                                 </div>
                             @endif
-                            {{-- <div class="row">
-                            <div class="col-md-8  col-offset-md-2 p-3">
-                                @if (Laravel\Fortify\Features::canUpdateProfileInformation())
-                                @livewire('profile.update-profile-information-form')
-                                <x-jet-section-border />
-                                @endif
-                            </div>
-                        </div> --}}
-                            <div class="row">
-                                <div class="col-md-8  col-offset-md-2 p-3">
-                                    @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
-                                        <div>
-                                            @livewire('profile.two-factor-authentication-form')
-                                        </div>
-                                    @endif
-                                </div>
-                            </div>
+
                             <div class="row">
                                 <div class="col-md-8  col-offset-md-2 p-3">
                                     <div class="mt-5">
