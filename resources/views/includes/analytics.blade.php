f041c1e8aea8a098e5da3f3fa2c96478d80454b6
Wadinga Leonard Ngonga <wadingaleonard@gmail.com>
2023-02-18 12:10:12 +0100

    Updated google analytics

--- resources/views/includes/analytics.blade.php
+++ resources/views/includes/analytics.blade.php
@@ -1,9 +1,12 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-HQPSPSLFV9"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-32NCDRJ57Z"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    window.dataLayer = window.dataLayer || [];
 
-  gtag('config', 'G-HQPSPSLFV9');
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-32NCDRJ57Z');
 </script>
