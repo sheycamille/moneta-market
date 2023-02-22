78841bade3f972e10dfa3dd92217504bc687f372
@camilleshey <wisdomcamille@gmail.com>
2023-02-22 12:00:37 +0100

    Fixed logout problem

--- resources/views/user/sidebar.blade.php
+++ resources/views/user/sidebar.blade.php
@@ -153,14 +153,10 @@
                     </li>
 
                     <li class="sidebar-item @yield('support-li')">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                        <a class="dropdown-item" href="{{ route('logout.perform') }}">
                             <i data-feather="log-out" class="log-out-icon"></i>
                             @lang('message.topmenu.log')
                         </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
                     </li>
 
                 </ul>
